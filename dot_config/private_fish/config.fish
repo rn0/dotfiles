5fce1e3a623c91c55ffc3f1fbbc9adf3078b3f4e
Piotr <rn0@users.noreply.github.com>
2022-05-08 18:03:40 +0200

    Add dot_config/private_fish/completions/fnm.fish Update dot_config/private_fish/config.fish Update run_once_install-packages.sh.tmpl

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,9 +3,12 @@ alias cat="bat --paging=never"
 
 set -Ux EDITOR micro
 
+fish_add_path $HOME/.fnm
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
     starship init fish | source
     atuin init fish | source
+    yq shell-completion fish | source
 end
